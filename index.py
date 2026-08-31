4c5187015bd36a1c2da6e47688989a9b3ffc76d6
SamGroup21 <ninzizasamuel20@gmail.com>
2026-08-31 23:03:22 +0200

    afficher,modifier les attribut qui ont été declarer prive,protege,par les methode : get,set et @property. notion sur l'heritage

--- index.py
+++ index.py
@@ -76,26 +76,153 @@
 # ou empecher les erreur en evitant qu'un attribut prenne une valeur impossible ou encore
 # controller comment les valeur vont etre modifier) #######################
  
-class Cours :  
-    def __init__(self,titre,etudiant,duree) :
-        self.titre = titre
-        self.__etudiant = etudiant
-        self.dure = duree
-        self._protege = 15
-        self.__prive = 10 #accessible seulement avec ou dans les methode de la classe
-    def afficher_info(self) :
+# class Cours :  
+#     def __init__(self,titre,etudiant,duree) :
+#         self.titre = titre
+#         self.__etudiant = etudiant
+#         self.dure = duree
+        # self._protege = 15
+        # self.__prive = 10 #accessible seulement avec ou dans les methode de la classe
+    # def afficher_info(self) :
         
-        print(f"Titre du cours : {self.titre}")
-        print("Etudiants : ",self.__etudiant)
-        print((f"Duree du cours : {self.dure} min"))
+        # print(f"Titre du cours : {self.titre}")
+        # print("Etudiants : ",self.__etudiant)
+        # print((f"Duree du cours : {self.dure} min"))
         
         
-c2 = Cours(titre = "django",etudiant = 0,duree =20)
-c2.afficher_info()
+# c2 = Cours(titre = "django",etudiant = 0,duree =20)
+# c2.afficher_info()
 # "_protege" in print(dir(c2))
 # "__prive" in print(dir(c2))
 
 # print(c2._protege)
 #print(c2.__prive) # ça ne va pas s'execter car la variable est privé
 # print(c2._Cours__prive) # accessible seulement avec ou dans les methode de la classe
+
+
+
+
+
+############## avoir acces ou modifier aux element protege, prive avec les methode get et set#################
+
+# class Cours :  
+#     def __init__(self,titre,etudiant,duree) :
+#         self.titre = titre
+#         self.dure = duree
+#         self.__etudiant = etudiant
+        
+        
+#     def get_etudiant(self) : 
+#         return self.__etudiant
+    
+#     def set_etudiant(self, valeur) :
+#         if valeur >= 0 :
+#             self.__etudiant = valeur
+#         else :
+#             print("Erreur :le nombre doit etre superieur ou egal a 0")
+        
+# c = Cours("100+ exercices en POO",120,120)
+# print(c.get_etudiant())
+# print((c.set_etudiant(-50)))
+# print(c.get_etudiant())
+
+############## avoir acces ou modifier aux element protege, prive avec les methode @property #################
+
+
+
+
+
+# class Cours :  
+#     def __init__(self,titre,etudiant,duree) :
+#         self.titre = titre
+#         self.dure = duree
+#         self.__etudiant = etudiant
+        
+#     @property   
+#     def etudiant(self) : 
+#         return self.__etudiant
+    
+#     @etudiant.setter
+#     def etudiant (self, valeur) :
+#         if valeur >= 0 :
+#             self.__etudiant = valeur
+#         else :
+#             print("Erreur :le nombre doit etre superieur ou egal a 0")
+        
+# c = Cours("100+ exercices en POO",20,120)
+# print(c.etudiant)
+# c.etudiant = 10000
+# print(c.etudiant)
+# c.etudiant = -120
+# print(c.etudiant)
+# c.etudiant = -120
+# print(c.etudiant)
+
+
+
+
+######### Heritage  ##################
+
+#exemple dans une entreprise
+
+class Employe :
+    def __init__(self,name,salary) : #,code_editor,designer) :
+        self.name = name
+        self.salary = salary
+        # self.code_editor = code_editor
+        # self.designer = designer
+        
+#     def partir_en_pause (self) :
+#         print(f"{self.name} part en pause")
+        
+        
+#     def coder(self) :
+#         print(f"{self.name} se met à coder")
+        
+#     def designer(self) :
+#         print(f"{self.name} se met à designer")
+        
+        
+        
+        
+# personne = Employe("Samuel",2000)
+# print(personne.partir_en_pause())
+
+
+# Toutes les autres classe vont heriter de la classe Employe
+    
+class Employe :
+    def __init__(self,name,salary) : 
+        self.name = name
+        self.salary = salary
+        
+    def partir_en_pause (self) :
+        print(f"{self.name} part en pause")   
+        
+class Developpeur(Employe) :
+    def __init__(self,name,salary,code_editor) :
+        super().__init__(name,salary) #fait reference les attribut de la la classe parent(employer)
+        self.code_editor = code_editor
+        
+    def coder(self) : 
+        print(f"{self.name} se met à coder")
+
+class Designer(Employe) :
+    def __init__(self,name,salary,designer) :
+        super().__init__(name,salary)
+        self.designer = designer
+        
+    def desining (self) :  
+        print(f"{self.name} se met a designer")
+       
+   
+codeur = Developpeur("bob",2000,"Visual studio code")
+designeur = Designer("samuel",2000,"Figma")
+
+print(codeur.code_editor)
+print(designeur.designer)
+print(codeur.coder())
+print(designeur.desining())
+
+
 
